adfd11157dcce9fd8cc29f732e9606278faeaa3d
lx4r <lx4r@users.noreply.github.com>
2024-02-25 13:36:08 +0100

    Show countries and cities on map

---
+++ main.py
@@ -0,0 +1,46 @@
+import folium
+import streamlit as st
+import toml
+import json
+from streamlit_folium import st_folium
+from geopy.geocoders import Nominatim
+
+st.set_page_config(layout="wide")
+
+data = toml.load("./countries.toml")
+
+geolocator = Nominatim(user_agent="countriesMap")
+
+
+# center on Liberty Bell, add marker
+m = folium.Map()
+
+# Load GeoJSON data
+with open("./countries_medium_resolution.geo.json") as f:
+    geojson_data = json.load(f)
+
+# Filter GeoJSON data to only include countries in countries.toml
+filtered_geojson_data = {
+    "type": "FeatureCollection",
+    "features": [
+        feature
+        for feature in geojson_data["features"]
+        if feature["properties"]["name"]
+        in [country["name"] for country in data["countries"]]
+    ],
+}
+
+# Add GeoJSON to map
+folium.GeoJson(filtered_geojson_data, name="geojson").add_to(m)
+
+for country in data["countries"]:
+    for city in country["cities"]:
+        location = geolocator.geocode(query={"city": city["name"]})
+        if location:
+            folium.Marker(
+                [location.latitude, location.longitude],
+                tooltip=city["name"],
+            ).add_to(m)
+
+# call to render Folium map in Streamlit
+st_folium(m, width=1000, returned_objects=[])
