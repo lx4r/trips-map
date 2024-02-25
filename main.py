20149eeea0dd2871977cb68b29c1581cfc06849c
lx4r <lx4r@users.noreply.github.com>
2024-02-25 14:32:19 +0100

    Cache country outlines, search for cities within countries

--- main.py
+++ main.py
@@ -7,25 +7,33 @@ from geopy.geocoders import Nominatim
 
 st.set_page_config(layout="wide")
 
-data = toml.load("./countries.toml")
-
 geolocator = Nominatim(user_agent="countriesMap")
 
 
+def load_countries_data():
+    return toml.load("./countries.toml")
+
+
 @st.cache_data
-def get_city_coordinates(city_name):
-    location = geolocator.geocode(query={"city": city_name})
+def get_city_coordinates(city_name, country_name):
+    location = geolocator.geocode(query={"city": city_name, "country": country_name})
     if location:
         return [location.latitude, location.longitude]
     else:
         return None
 
 
-m = folium.Map()
+@st.cache_data
+def load_geojson_data():
+    with open("./countries_medium_resolution.geo.json") as f:
+        return json.load(f)
 
-# Load GeoJSON data
-with open("./countries_medium_resolution.geo.json") as f:
-    geojson_data = json.load(f)
+
+countries_data = load_countries_data()
+
+geojson_data = load_geojson_data()
+
+m = folium.Map()
 
 # Filter GeoJSON data to only include countries in countries.toml
 filtered_geojson_data = {
@@ -34,16 +42,20 @@ filtered_geojson_data = {
         feature
         for feature in geojson_data["features"]
         if feature["properties"]["name"]
-        in [country["name"] for country in data["countries"]]
+        in [country["name"] for country in countries_data["countries"]]
     ],
 }
 
 # Add GeoJSON to map
 folium.GeoJson(filtered_geojson_data, name="geojson").add_to(m)
 
-for country in data["countries"]:
+for country in countries_data["countries"]:
+    if not country.get("cities"):
+        continue
     for city in country["cities"]:
-        coordinates = get_city_coordinates(city["name"])
+        coordinates = get_city_coordinates(
+            city_name=city["name"], country_name=country["name"]
+        )
         if coordinates:
             folium.Marker(
                 coordinates,
